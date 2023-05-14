78066894028d2f32cb033ea905a79c01fef5d4db
marcelo <joao69@aluno.senai-ce.org.br>
2023-05-14 14:15:36 -0300

    adicionando mais eventos

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+
     }
 
     initialize(){
@@ -21,15 +22,25 @@ class CalcController {
         }, 1000);
 
     }
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+
+    }
 
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
             
-            btn.addEventListener('click', e =>{
+            this.addEventListenerAll(btn, "click drag", e =>{
                 console.log(btn.className.baseVal.replace("btn-",""));
 
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
             });
         });
     }
